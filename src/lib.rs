e6f1d54527704b86070f2fc2061e8386ada460b8
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2024-01-28 05:44:27 +0800

    added the implementation for result

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,12 @@ impl<T> traits::RightNow<T> for std::option::Option<T> {
         self.expect(msg)
     }
 }
+
+impl<T, E: std::fmt::Debug> traits::RightNow<T> for std::result::Result<T, E> {
+    fn runwrap(self) -> T {
+        self.unwrap()
+    }
+    fn rexpect(self, msg: &str) -> T {
+        self.expect(msg)
+    }
+}
